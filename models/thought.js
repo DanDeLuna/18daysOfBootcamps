9bda47233b5c0b654a144dec1371604883cf23af
DanDeLuna <danieldeluna4@gmail.com>
2023-06-23 17:51:49 -0600

    models thoughs and index.js files made

---
+++ models/thought.js
@@ -0,0 +1,68 @@
+const { Schema, model, Types } = require('mongoose');
+
+const moment = require('moment')
+
+const reactSchema = new Schema (
+  {
+     reactId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+     },
+     reactBody: {
+      type: String,
+      required: true,
+      maxlength: 295
+     },
+     username: {
+      type: String,
+      required: true,
+     },
+     madeAt: {
+      type: Date,
+      default: Date.now,
+      get: madeAtVal => moment(madeAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+     },},
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true
+      },
+      id: false,
+  }
+)
+
+const thoughtsSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      madeAt: {
+        type: Date,
+        default: Date.now,
+        get: madeAtVal => moment(madeAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+thoughtSchemas.virtual('reactCount')
+.get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', thoughtsSchema);
+module.exports = Thought;
