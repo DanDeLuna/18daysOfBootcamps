3df1fb5e3d6c89575eb147d1e0bb0d6aecbf58ee
DanDeLuna <danieldeluna4@gmail.com>
2023-06-24 20:19:36 -0600

    user model made

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const { Schema, model, Types } = require('mongoose');
+
+const usuarioSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Enter a  valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    amigo: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+
+
+usuarioSchema.virtual("amigoCount").get(function () {
+  return this.amigo.length;
+});
+const Usuario = model('User', usuarioSchema);
+module.exports = Usuario;
