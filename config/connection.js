8f8ed753b1824100450a97d48444a06b4568e312
DanDeLuna <danieldeluna4@gmail.com>
2023-06-25 21:42:52 -0600

    Connection debug

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/socialMedia",
+  process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/NewsocialMedia",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
